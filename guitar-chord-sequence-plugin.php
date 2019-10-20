e71102f766d8473673e056db7669c01e0c56f0bb
unknown <codeleap@outlook.com>
2019-10-20 08:32:10 +0200

    Plugin details added to main file.

--- guitar-chord-sequence-plugin.php
+++ guitar-chord-sequence-plugin.php
@@ -1,2 +1,11 @@
 <?php
+/*
+* Plugin Name: Guitar Chord Sequence Plugin
+* Plugin URI: http://wprevs.com
+* Description: A plugin for guitarists that creates a page where chord sequences of different lengths are generated at the click of a button. It can be useful for practicing the guitar or even writing songs.   
+* Author: wprevs.com
+* Version: 1.0.0
+* Author URI: http://wprevs.com
+* License: GPLv2 or later
+*/
 
