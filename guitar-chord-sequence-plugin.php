90525687ede20a8df2ca93d56beb517b5f9dd2a8
unknown <codeleap@outlook.com>
2019-10-25 16:18:59 +0200

    Button ids added to display chord grouping buttons.

--- guitar-chord-sequence-plugin.php
+++ guitar-chord-sequence-plugin.php
@@ -11,13 +11,13 @@
 
 if(!defined( 'ABSPATH' )) exit;
 
-
-
-//Here we add in our init classes for registering CPT and metaboxes
+//Here we add in our init classes for registering CPT, metaboxes and js, css etc.
 require_once
 dirname( __FILE__ ) . '/classes-init/class-gcs-custom-post-type-initializer.php';
+require_once
+dirname( __FILE__ ) . '/classes-init/class-gcs-scripts-initializer.php';
 
-//$test = new GCS_Custom_Post_Type_Initializer();
+$gcs_scripts_initializer = new GCS_Scripts_Initializer( get_chord_groupings() );
 
 function get_chord_groupings(){
     $chord_groupings = array(
@@ -67,40 +67,18 @@ function display_chords( $chord_groupings ){
     }
     return $output;
 }
-/*
-function display_chord_grouping_buttons(){
-    $chord_groupings = get_chord_groupings();
-
-    $chord_grouping_buttons = '';
-    $output = '';
-
-    foreach( $chord_groupings as $chord_grouping ){
-        $admin_post_url = esc_url( admin_url('admin-post.php') );
-        $chord_grouping_buttons .= '<form method="post" action="' . get_permalink() . '">';
-        $chord_grouping_buttons .= '<input type="submit" id="chord_grouping" name="chord_grouping" value="' . 
-        $chord_grouping['name'] . '">';
-        $chord_grouping_buttons .= '</form>'; 
-    }
-
-
-    $output .= display_chords( $chord_groupings );
-
-    return $output . $chord_grouping_buttons;
-}
-*/
 
 function display_chord_grouping_buttons(){
     $html = '';
     foreach( get_chord_groupings() as $chord_grouping ){
         $html .= '<div>';
-        $html .= '<button>' . $chord_grouping['name'] . '</button>';
+        $html .= '<button class="chord-button" '; 
+        $html .= 'id="button' . $chord_grouping['name'] . '">';
+        $html .= $chord_grouping['name'] . '</button>';
         $html .= '</div>';
     }
+    $html .= '<div id="chordSequenceDisplayDiv"></div>';
     return $html;
-}
-
-function send_php_array_to_javascript(){
-    //Here we use localize_script to send our php to be used in a JS file.
 }
 
 add_shortcode( 'display_chord_buttons', 'display_chord_grouping_buttons' );
