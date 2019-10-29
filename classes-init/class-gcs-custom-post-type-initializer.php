d405d8ce7bb21b16f1a8ee9881a5519840d328c0
unknown <codeleap@outlook.com>
2019-10-29 18:22:50 +0100

    Method add_chord_grouping_post_type added.

--- classes-init/class-gcs-custom-post-type-initializer.php
+++ classes-init/class-gcs-custom-post-type-initializer.php
@@ -3,4 +3,8 @@ class GCS_Custom_Post_Type_Initializer{
     public function __construct(){
         add_action( 'init', array( $this, 'add_chord_grouping_post_type' ));
     }
+
+    public function add_chord_grouping_post_type(){
+    	
+    }
 }
