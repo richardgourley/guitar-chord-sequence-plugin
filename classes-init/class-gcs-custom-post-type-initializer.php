4c4ddb689f9c16b2b2e76ca45d7091c0a1eec125
unknown <codeleap@outlook.com>
2019-10-30 20:05:08 +0100

    CPT name changed to key chord grouping.

--- classes-init/class-gcs-custom-post-type-initializer.php
+++ classes-init/class-gcs-custom-post-type-initializer.php
@@ -1,10 +1,15 @@
 <?php
 class GCS_Custom_Post_Type_Initializer{
     public function __construct(){
-        add_action( 'init', array( $this, 'add_chord_grouping_post_type' ));
+        add_action( 'init', array( $this, 'add_key_chord_grouping_post_type' ));
     }
 
-    public function add_chord_grouping_post_type(){
-    	
+    //To be called once on plugin activation
+    public function rewrite_rules(){
+
+    }
+
+    public function add_key_chord_grouping_post_type(){
+
     }
 }
