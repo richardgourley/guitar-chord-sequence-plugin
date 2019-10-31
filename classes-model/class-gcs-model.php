6cc8cec0617f833cdf6cd75fa98f76ec09ab45f8
unknown <codeleap@outlook.com>
2019-10-31 20:47:46 +0100

    WP Query for key_chord_grouping added to get_chord_groupings method.

--- classes-model/class-gcs-model.php
+++ classes-model/class-gcs-model.php
@@ -4,9 +4,12 @@ class GCS_Model{
 	public function __construct( ){
 		
 	}
-
+    
+    /************
+    * @returns: Array - chord groupings with post meta saved in an array
+    *************/
     public function get_chord_groupings(){
-        
+        $query = new WP_Query( 'post_type' => 'key_chord_grouping' );
     }
 
 	
