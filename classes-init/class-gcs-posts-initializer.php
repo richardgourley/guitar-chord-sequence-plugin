fac8437bc43f5c9af0a95903723aaab65ed787d2
unknown <codeleap@outlook.com>
2019-10-31 18:36:33 +0100

    Post status set to publish on generate_post method.

--- classes-init/class-gcs-posts-initializer.php
+++ classes-init/class-gcs-posts-initializer.php
@@ -13,7 +13,7 @@ class GCS_Posts_Initializer{
     * Post meta is then added to an array that is passed onto our JS file that generates chord sequences
     **********/
     public function generate_posts(){
-        generate_post(
+        $this->generate_post(
             'Key of C', 
             array(
                 'root_major' => 'C',
@@ -32,11 +32,12 @@ class GCS_Posts_Initializer{
     * Inserts a post with the paramaters provided
     ************/
     private function generate_post( $post_title, $meta_input ){
-        //post_type and comment_status set without params
+        //post_type, post_status and comment_status set without params
         $post_array = array(
             'post_title'     => $post_title,
             'post_type'      => 'key_chord_grouping',
             'comment_status' => 'closed',
+            'post_status'    => 'publish',
             'meta_input'     => $meta_input
         );
         wp_insert_post( $post_array );
