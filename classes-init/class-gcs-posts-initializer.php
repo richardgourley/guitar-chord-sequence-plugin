fe03baf1b9a095366397de102ec88d5cfccb426f
unknown <codeleap@outlook.com>
2019-10-31 17:52:22 +0100

    Params and return comments added to generate_post method

--- classes-init/class-gcs-posts-initializer.php
+++ classes-init/class-gcs-posts-initializer.php
@@ -13,7 +13,16 @@ class GCS_Posts_Initializer{
     * Post meta is then added to an array that is passed onto our JS file that generates chord sequences
     **********/
     public function generate_posts(){
-        
+
+    }
+    
+    /***********
+    * @params = post_title, post_type, comment_status, meta_input (ARRAY OF CHORDS AS POST META)
+    * returns - empty
+    * Inserts a post with the paramaters provided
+    ************/
+    private function generate_post(){
+
     }
 
 	
