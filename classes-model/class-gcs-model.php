0cbaba85135e75e82c96459196557496ac0494d8
unknown <codeleap@outlook.com>
2019-10-31 21:15:30 +0100

    Post meta added to chord grouping array.

--- classes-model/class-gcs-model.php
+++ classes-model/class-gcs-model.php
@@ -10,7 +10,16 @@ class GCS_Model{
     *************/
     public function get_chord_groupings(){
         $chord_groupings = [];
-        $query = new WP_Query( 'post_type' => 'key_chord_grouping' );
+        $pt_query = new WP_Query( array( 'post_type' => 'key_chord_grouping' ) );
+
+        foreach( $pt_query->posts as $key_chord_grouping ){
+            $chord_grouping = [];
+            $chord_grouping['title'] = $key_chord_grouping->post_title;
+            $post_meta = get_post_meta( $key_chord_grouping->ID );
+            $chord_grouping['post_meta'] = $post_meta;
+            array_push( $chord_groupings, $chord_grouping );
+        }
+        return $chord_groupings;
     }
 
 	
