45130512e7324c3e1263c3e34a5221a15a2faad6
unknown <codeleap@outlook.com>
2019-10-31 17:45:13 +0100

    Explanation of key chord groupings added to generate_posts method.

--- classes-init/class-gcs-posts-initializer.php
+++ classes-init/class-gcs-posts-initializer.php
@@ -4,6 +4,17 @@ class GCS_Posts_Initializer{
 	public function __construct( ){
 		
 	}
+    
+    /*********
+    * Generates a 'key_chord_grouping' custom post type with a post_title of a key name (C, G, D)
+    * Adds nothing to post_content
+    * Adds chord names as post meta to the post
+    * Each post is searchable by post_type
+    * Post meta is then added to an array that is passed onto our JS file that generates chord sequences
+    **********/
+    public function generate_posts(){
+        
+    }
 
 	
 }
