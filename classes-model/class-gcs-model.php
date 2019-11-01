80fc522e0fe5221f3c502fa7d494599587f67866
unknown <codeleap@outlook.com>
2019-11-01 16:13:58 +0100

    get_chord_groupings changed to return test array

--- classes-model/class-gcs-model.php
+++ classes-model/class-gcs-model.php
@@ -9,17 +9,21 @@ class GCS_Model{
     * @returns: Array - chord groupings with post meta saved in an array
     *************/
     public function get_chord_groupings(){
+        /*
         $chord_groupings = [];
-        $pt_query = new WP_Query( array( 'post_type' => 'key_chord_grouping' ) );
+        $query = new WP_Query( array( 'post_type' => 'key_chord_grouping' ) );
 
-        foreach( $pt_query->posts as $key_chord_grouping ){
+        foreach( $query->posts as $key_chord_grouping ){
             $chord_grouping = [];
             $chord_grouping['title'] = $key_chord_grouping->post_title;
             $post_meta = get_post_meta( $key_chord_grouping->ID );
             $chord_grouping['post_meta'] = $post_meta;
             array_push( $chord_groupings, $chord_grouping );
         }
-        return $chord_groupings;
+        */
+
+        //return $chord_groupings;
+        return array(1,2,3,4,5,6,7,8,9,10);
     }
 
 	
