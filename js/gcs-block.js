430878bd23871d72960bd4f9a977e2cc9c662dc4
unknown <codeleap@outlook.com>
2019-11-14 20:16:56 +0100

    JS block file added for producing the code for the display block.

---
+++ js/gcs-block.js
@@ -0,0 +1,86 @@
+if(document.getElementById("chordSequenceButtonsDiv")){
+
+    let chordSequenceButtonsDiv = 
+    document.getElementById("chordSequenceButtonsDiv");
+
+    let chordSequenceDisplayDiv = 
+    document.getElementById("chordSequenceDisplayDiv");
+
+    //assign a button for every chord grouping title
+    chordSequenceButtonsDiv.innerHTML = createButtons();
+    
+    //assign an action for each button onclick
+    assignButtonActions();
+
+}
+
+function createButtons(){
+    let html = '';
+    let chordGroupingButtons = '';
+    for(i=0; i<chordGroupings.length; i++){
+        html += '<div class="chord-sequence-buttons-div">';
+        html += '<button id="' + chordGroupings[i].title + '">' + chordGroupings[i].title + '</button>';
+        html += '</div>';
+    }
+    return html;
+}
+
+function assignButtonActions(){
+    for(i=0; i<chordGroupings.length; i++){
+        document.getElementById(chordGroupings[i].title).onclick = function(){
+            printChords(getChords(this.id));
+        }
+    }
+}
+
+//gets all chords and returns as an object for matching id/title
+function getChords(id){
+	let obj = {};
+	for(i=0; i<chordGroupings.length; i++){
+		if(id === chordGroupings[i].title){
+            obj.rootMajor = chordGroupings[i].root_major;
+			obj.rootMinor = chordGroupings[i].root_minor;
+            obj.major2 = chordGroupings[i].major2;
+            obj.major3 = chordGroupings[i].major3;
+            obj.minor2 = chordGroupings[i].minor2;
+            obj.minor3 = chordGroupings[i].minor3;
+            return obj;
+		}
+	}
+	return null;
+}
+
+function printChords(obj){
+    let rootChord = '';
+    //Decide if starting with ROOT MAJOR or ROOT MINOR using random number
+    let majorOrMinor = Math.random();
+    if(majorOrMinor >= 0.5){
+        rootChord += obj.rootMajor;
+    }else{
+        rootChord += obj.rootMinor;
+    }
+    //shuffle rest of chords
+    let shuffledChords = shuffleChords([obj.major2, obj.major3, obj.minor2, obj.minor3]);
+    
+    //Display 2,3 and 4 chord sequences
+    chordSequenceDisplayDiv.innerHTML = 
+        '<h2>2 chords: ' + rootChord + ' ' + shuffledChords[0] + '</h2>' + 
+        '<h2>3 chords: ' + rootChord + ' ' + shuffledChords[0] + ' ' + shuffledChords[1] + '</h2>' + 
+        '<h2>4 chords: ' + rootChord + ' ' + shuffledChords[0] + ' ' + shuffledChords[1] + ' ' + shuffledChords[2] + '</h2>';
+}
+
+function shuffleChords(chords){
+    //Fisher Yates shuffle algorithm
+    var j, x, i;
+    for (i = chords.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = chords[i];
+        chords[i] = chords[j];
+        chords[j] = x;
+    }
+    return chords;
+}
+
+
+
+
